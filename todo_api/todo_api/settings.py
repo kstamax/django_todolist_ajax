162991206c39169df4506098c2c78db3467930a9
kstamax <poberezhnyimax02@gmail.com>
2022-09-27 20:12:53 +0300

    login and register pages updated

--- todo_api/todo_api/settings.py
+++ todo_api/todo_api/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api.apps.ApiConfig',
-    'main.apps.MainConfig',
+    'api',
+    'main',
 ]
 
 MIDDLEWARE = [
